bc7bb0ad14e5fbd396ab76c471ca1a7008d3022f
khallad2 <khallad_3@hotmail.com>
2024-01-10 17:14:11 +0100

    feat: fix update as invitation and from menu

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
-import {StrictMode} from "react";
 import ErrorBoundary from "./components/common/ErrorBoundry.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <StrictMode>
         <BrowserRouter>
             <ErrorBoundary>
                 <App />
             </ErrorBoundary>
         </BrowserRouter>
-    </StrictMode>
 )
